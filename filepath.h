a89a24dec6683c449b82afb11096f0114c3b6c88
libbylg <libbylg@126.com>
2018-10-08 08:17:57 +0800

    定义一个专用的filepath接口，进一步扩展fliepath的能力

--- filepath.h
+++ filepath.h
@@ -23,6 +23,23 @@ EXTERN  char*   basename(char* p);
 #endif
 
 
+struct filepath_t
+{
+
+};
+
+void    filepath_exit       (struct filepath_t* p);
+int     filepath_init       (struct filepath_t* p, char* path);
+int     filepath_abs        (struct filepath_t* p, char* path);
+int     filepath_clean      (struct filepath_t* p, char* path);
+int     filepath_join       (struct filepath_t* p, char* elem, ...);
+
+
+char*   filepath_print      (struct filepath_t* p);
+char*   filepath_dirname    (struct filepath_t* p);
+char*   filepath_basename   (struct filepath_t* p);
+char*   filepath_ext        (struct filepath_t* p);
+int     filepath_split      (struct filepath_t* p, int (*callback)(void* context, char* elem). void* context);
 
 
 #endif//__filepath_H_

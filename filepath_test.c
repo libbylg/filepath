831e91f07cc89a2a7ef2c1aae07af0ebc4614183
libbylg <libbylg@126.com>
2018-11-10 08:46:17 +0800

    实现和linux兼容的basename和dirname

--- filepath_test.c
+++ filepath_test.c
@@ -5,34 +5,53 @@
 
 #include "filepath.h"
 
+#include "cmocka.h"
 
-#define TEST_SET    \
-    TEST_CASE("/", "/", "/")    \
-TEST_CASE("/usr/lib",    "/usr",    "lib")  \
-TEST_CASE("/usr/"   ,   "/"    ,    "usr")  \
-TEST_CASE("/usr"    ,   "/"    ,    "usr")  \
-TEST_CASE("usr"     ,    "."   ,    "usr")  \
-TEST_CASE("."       ,    "."   ,    ".")    \
-TEST_CASE(".."      ,    "."   ,    "..")
+#include "stdlib.h"
+
+
+
+
+#define FILEPATH_TEST(scene_name,p,dir,base)  \
+TEST(scene_name) \
+{ \
+    char* t = NULL; \
+    \
+    printf("%s\n", p); \
+    \
+    t = strdup(p); \
+    char* b = basename(t); \
+    assert_string_equal(base, b); \
+    free(t); \
+    \
+    t = strdup(p); \
+    char* d = dirname(t); \
+    assert_string_equal(dir, d); \
+    free(t); \
+}
+
+
+
+
+FILEPATH_TEST(root,             "/"         ,   "/"     ,   "/")
+FILEPATH_TEST(normal,           "/usr/lib"  ,   "/usr"  ,   "lib")
+FILEPATH_TEST(end_with_pathspr, "/usr/"     ,   "/"     ,   "usr")
+FILEPATH_TEST(file_in_root,     "/usr"      ,   "/"     ,   "usr")
+FILEPATH_TEST(no_pathspr,       "usr"       ,   "."     ,   "usr")
+FILEPATH_TEST(cur,              "."         ,   "."     ,   ".")
+FILEPATH_TEST(parent,           ".."        ,   "."     ,   "..")
+FILEPATH_TEST(relative,         "usr/tm.f"  ,   "usr"   ,   "tm.f")
+FILEPATH_TEST(more_pathspr,     "/usr///"   ,   "/"     ,   "usr")
+
+
 
 
 int main(int argc, char* argv[])
 {
-
-#define TEST_CASE(p,d,b)                       \
-    {                                          \
-        printf("test: %s\n", p);               \
-        char* t = strdup(p);                   \
-        assert(0 == strcmp(d, dirname(t)));    \
-                                               \
-        t = strdup(p);                         \
-        assert(0 == strcmp(b, basename(t)));   \
-    }
-
-    TEST_SET
-
-#undef TEST_CASE
-
-        return 0;
+    TEST_RUN();
+    getchar();
+    return 0;
 }
+
+
 
